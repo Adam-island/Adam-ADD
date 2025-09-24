cdf00307d790ae5389f3bbea1c55ee3e3f443ea4
Adam-island <adamgoyes@gmail.com>
2025-09-24 14:52:03 +0100

    Script

---
+++ Adambash.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+menu(){
+  opcion=1
+  while [ $opcion -ne 0 ]; do
+  # Menú que se muestra por pantalla
+    echo "**"
+    echo "Opción 1: Bisiesto"
+    echo "Opción 2: Configurar red"
+    echo "Opción 3: Adivina"
+    echo "Opción 4: Buscar"
+    echo "Opción 5: Contar"
+    echo "Opción 6: Permiso octal"
+    echo "Opción 7: Romano"
+    echo "Opción 8: Automatizar"
+    echo "Opción 9: Crear"
+    echo "Opción 10: Crear 2"
+    echo "Opción 11: Reescribir"
+    echo "Opción 12: Contusu"
+    echo "Opción 13: Quita blancos"
+    echo "Opción 14: Lineas"
+    echo "Opción 15: Analizar"
+    echo "Opción 0: Para salir"
+    echo "**"
+    read -p "Elegir la opción deseada: " opcion
+    echo ""
+    case $opcion in
+      0)
+      ;;
+      1)
+        read -p "Dame un año: " anyo
+        let anyo=anyo-1
+        if (( ( $anyo % 4 == 0  &&  $anyo % 100 != 0 ) || ( $anyo % 400 == 0 ) )); then
+          echo "$anyo es un año bisiesto"
+        else
+          echo "$anyo no es un año bisiesto"
+        fi
+        sleep 5
+      ;;
+      2)
+        read -p "Dame la ip: " ip
+        read -p "Dame la mascara: " masc
+        read -p "Dame la puerta de enlace: " enlace
+        read -p "Dame el dns: " dns
+        cat > /etc/netplan/50-cloud-init.yaml <<EOF
+        network:
+          version: 2
+          renderer: NetworkManager
+          ethernets:
+            enp0s3:
+              addresses: [$ip/$masc]
+              routes:
+               - to: default
+                 via: $enlace
+              nameservers:
+                addresses: [$dns]
+EOF
+        cat /etc/netplan/50-cloud-init.yaml
+      ;;
+      3)
+        aleatorio=$(( RANDOM % 2 ))
+        intentos=5
+        total=1
+        for ((i=1; i<=5;i++)); do
+          echo "El número de intentos que tienes ahora es $intentos"
+          read -p "Adivina el número: " numusu
+          if [ $numusu -eq $aleatorio ]; then
+            echo "Has acertado campeon, yeray te aprueba!!"
+            break
+          elif [ $numusu -lt $aleatorio ]; then
+            echo "Tu número es mayor"
+            let intentos=$intentos-1
+            let total=$total+1
+          else
+            echo "Tu número es menor"
+            let intentos=$intentos-1
+            let total=$total+1
+          fi
+        done
+          if [ $numusu -eq $aleatorio ]; then
+            echo "El numero de intentos que has realizado es $total"
+          else
+            echo "Ya no te quedan intentos, el numero era el $aleatorio"
+          fi
+      ;;
+      4)
+      read -p "Dame el nombre de un fichero a buscar: " fichero
+        buscar=$(sudo find / -name "$fichero")
+          if [ -n "$buscar" ]; then
+            vocales=$(grep -o -i '[aeiou]' $buscar | wc -l)
+            echo "La ubicación es $buscar y tiene $vocales vocales"
+          else
+            echo "No se encontró el fichero"
+          fi
+      ;;
+      5)
+      ;;
+      6)
+      ;;
+      7)
+      ;;
+      8)
+      ;;
+      9)
+      ;;
+      10)
+      ;;
+      11)
+      ;;
+      12)
+      ;;
+      13)
+      ;;
+      14)
+      ;;
+      15)
+      ;;
+      *)
+        echo "Opción incorrecta"
+    esac
+  done
+}
+
+menu
